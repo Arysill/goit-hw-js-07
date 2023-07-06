9a11260100d31bd08c8dbf97b61d3e47840d94fa
Arysill <125421389+Arysill@users.noreply.github.com>
2023-07-06 01:32:36 -0500

    first commit

---
+++ js/02-lightbox.js
@@ -0,0 +1,32 @@
+
+import { galleryItems } from './gallery-items.js';
+
+const refs = {
+    listEl: document.querySelector('.gallery'),
+};
+
+function createGalleryItemsMarkup(arr) {
+    return arr
+        .map(({ preview, original, description }) =>
+            `<li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+       <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}">
+  </a>
+</li>`
+        )
+        .join('');
+}
+    refs.listEl.insertAdjacentHTML(
+        'beforeend',
+        createGalleryItemsMarkup(galleryItems)
+    );
+
+    const lightbox = new SilmpleLighbox('.gallery li a', {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    });
